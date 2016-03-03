5f387c01042104a3127611333bdda4a5cc55f4a1
Julien Rouviere <julien.rouviere@gmail.com>
2016-03-03 22:49:31 +0100

    Add a very crude layout of the tic tac toe board

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+class Board extends React.Component {
+    render() {
+        let rows = [];
+        for(let i=0; i<3; i++) {
+            rows.push(<div className="row">
+                <div className="cell">&#x2718;</div>
+                <div className="cell">&#x2718;</div>
+                <div className="cell">&#x25cb;</div>
+            </div>);
+        }
+        return <div>{rows}</div>
+    }
+}
+
 class App extends React.Component {
     render() {
-        return <h1>Hello react!</h1>
+        return <Board />
     }
 }
 
