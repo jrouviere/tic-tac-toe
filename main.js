e714de710264d7367b5d93025c52d6ce3fab83db
Julien Rouviere <julien.rouviere@gmail.com>
2016-03-03 23:35:54 +0100

    Move board state to component props

--- main.js
+++ main.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+var BOARD_STATE = [
+    [true, true, null],
+    [null, false, false],
+    [true, false, true],
+];
+
+class Cell extends React.Component {
+    render() {
+        let value = "cell";
+        if(this.props.value === true) {
+            value = "cell cross";
+        } else if(this.props.value === false) {
+            value = "cell circle";
+        }
+        return <div className={value} />
+    }
+}
 
 class Board extends React.Component {
     render() {
         let rows = [];
-        for(let i=0; i<3; i++) {
-            rows.push(<div className="row">
-                <div className="cell">&#x2718;</div>
-                <div className="cell">&#x2718;</div>
-                <div className="cell">&#x25cb;</div>
-            </div>);
+        for(let boardRow of this.props.boardState) {
+            let row = [];
+            for(let boardCell of boardRow) {
+                row.push(<Cell value={boardCell} />);
+            }
+            rows.push(<div className="row">{row}</div>)
         }
         return <div>{rows}</div>
     }
@@ -18,7 +35,7 @@ class Board extends React.Component {
 
 class App extends React.Component {
     render() {
-        return <Board />
+        return <Board boardState={BOARD_STATE}/>
     }
 }
 
